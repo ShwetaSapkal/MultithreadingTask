ed3eb9e2ee9950557d090842b237d023a4acca50
ShwetaSapkal <shwetarhs2000@gmail.com>
2022-05-31 11:14:39 +0530

    Add project files.

---
+++ MultithreadingTask/MultiPrgm1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultithreadingTask
+{
+    class MultiPrgm1
+    {
+        public void Run()
+        {
+            Thread t = Thread.CurrentThread;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(t.Name);
+                Console.WriteLine(i);
+                Thread.Sleep(500); // ms
+            }
+        }
+
+
+
+    }
+}
---
+++ MultithreadingTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultithreadingTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
